a165fe37360e2dd122eb83f83db17ed844e18b4b
arunjosephmathew <104261060+arunjosephmathew@users.noreply.github.com>
2022-07-28 11:25:54 +0530

    Converstion of BST to RightSkewTree

---
+++ Ques_2/src/com/greatlearning/main/DriverClass.java
@@ -0,0 +1,26 @@
+package com.greatlearning.main;
+
+import com.greatlearning.model.*;
+import com.greatlearning.service.*;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RightSkewTree tree = new RightSkewTree();
+		
+		tree.root = new Node(50);
+		tree.root.left = new Node(30);
+		tree.root.right = new Node(60);
+		tree.root.left.left = new Node(10);
+		tree.root.right.left =new Node(55);
+		
+		
+		
+		tree.converstion(tree.root);
+		tree.inOrder(tree.headNode);
+
+	}
+
+}
---
+++ Ques_2/src/com/greatlearning/model/Node.java
@@ -0,0 +1,14 @@
+package com.greatlearning.model;
+
+public class Node {
+	public int data;
+	public Node left,right;
+	
+	public Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+	
+	
+}
